1ed132fb86f6121876d61d74b35cba9e55f90ecd
Johnson Sebire <johnson@manifestghana.com>
2021-02-26 22:31:23 +0000

    Completed User Registration Layout Template

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,222 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.auth')
+@section('page-title', 'User Registration')
+@section('form')
+<body class="font-muli theme-cyan gradient">
 
-        <x-jet-validation-errors class="mb-4" />
+    <div class="auth option2">
+        <div class="auth_left">
+            <div class="card">
+                <div class="card-body">
+                    <div class="text-center">
+                        <a class="header-brand" href="{{route('dashboard')}}">
+                            <img src="{{asset('images/favicon.png')}}" class="brand-logo" width="50px" height="50px">
+                            {{-- <i class="fa fa-graduation-cap brand-logo"></i> --}}
+                        </a>
+                        <div class="card-title mt-3">Create new account</div>
+                    <!--    <button type="button" class="btn btn-facebook"><i class="fa fa-facebook mr-2"></i>Facebook</button>
+                        <button type="button" class="btn btn-google"><i class="fa fa-google mr-2"></i>Google</button>
+                        <h6 class="mt-3 mb-3">Or</h6>
+                    --> 
+                    </div>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <div class="form-group">
+                          
+                            <input 
+                            type="text" 
+                            class="form-control @error('name') is-invalid @enderror" 
+                            placeholder="Enter name"
+                            name="name" 
+                            value="{{ old('name') }}" 
+                            required autocomplete="name" 
+                            autofocus
+                            >
+
+                            @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+                        </div>
+                        <div class="form-group">
+                       
+                            <input 
+                            type="email" 
+                            class="form-control @error('email') is-invalid @enderror"  
+                            placeholder="Enter email"
+                            
+                            name="email" value="{{ old('email') }}" 
+                            required autocomplete="email"
+                            >
+
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+
+                        </div>
+                        <div class="form-group">
+                        
+                            <input 
+                            type="password" 
+                            class="form-control @error('password') is-invalid @enderror" 
+                            placeholder="Password"
+                            name="password" 
+                            required autocomplete="new-password"
+                            >
+
+                            @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                            
+
+                        </div>
+                        <div class="form-group"> 
+                         
+                            <input 
+                                type="password" 
+                                class="form-control" 
+                                name="password_confirmation" 
+                                required autocomplete="new-password"
+                                placeholder="Confirm Password"
+                                >
+
+                        </div> 
+                        <div class="form-group">
+                            <label class="custom-control custom-checkbox"> 
+                            
+                           <input 
+                           type="checkbox" 
+                           class="custom-control-input" 
+                           
+                           />
+                            
+                           <span class="custom-control-label">Agree to <a href="{{route('terms.show')}}">terms of use policy</a></span>
+                            </label>
+                        </div>
+                        <div class="text-center">
+
+                            <button type="submit" class="btn btn-primary btn-block">Create new account</button>
+
+                            
+
+                            <div class="text-muted mt-4">Already have account? <a href="{{route('login')}}">Sign in</a></div>
+                        </div>
+                    </form>
+                </div>
             </div>
+        </div>
+    </div>
+    
+@endsection
 
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+@section('content')
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
 
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms"/>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
-                                ]) !!}
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6"> 
+
+                                <input 
+                                id="name" 
+                                type="text" 
+                                class="form-control @error('name') is-invalid @enderror" 
+                                name="name" 
+                                value="{{ old('name') }}" required 
+                                autocomplete="name" 
+                                autofocus
+                                >
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
-                    </x-jet-label>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+
+                                <input 
+                                id="email" 
+                                type="email" 
+                                class="form-control @error('email') is-invalid @enderror" 
+                                name="email" value="{{ old('email') }}" 
+                                required autocomplete="email"
+                                >
+ 
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input 
+                                id="password" 
+                                type="password" 
+                                class="form-control @error('password') is-invalid @enderror" 
+                                name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6"> 
+                                <input 
+                                id="password-confirm" 
+                                type="password" 
+                                class="form-control" 
+                                name="password_confirmation" 
+                                required autocomplete="new-password">
+                            </div>
+                        </div>
+                    
+                    
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+    </div>
+</div>
+@endsection
