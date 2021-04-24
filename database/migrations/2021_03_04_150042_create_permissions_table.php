f3584f292a4355689c2d9f8bd401fd2148f914f5
Johnson Sebire <johnson@manifestghana.com>
2021-04-24 14:07:46 +0000

    Added All Migrations

---
+++ database/migrations/2021_03_04_150042_create_permissions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('permission', 255)->nullable(); 
+            $table->string('school_id',255)->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
